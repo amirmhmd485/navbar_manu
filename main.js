392972f6c3cdf049507556ac55e5a2bcd1b5b0e4
amirmhmd485 <amirmhmd378@gmail.com>
2026-04-22 01:22:22 +0200

    fix Issue

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ const ul = document.querySelector("ul");
 let isOpen = true;
 function openNavBar(){
     if(isOpen){
-        ul.style.height = '215px';
+        ul.style.height = 'auto';
         isOpen = false;
     }
     else{
         ul.style.height = '0';
         isOpen = true;
+        window.addEventListener("resize" ,function(e){
+            ul.style.height = "auto";
+        })
     }
 }
+
 icon.addEventListener("click" , openNavBar);
